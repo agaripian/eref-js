5a4681b391c49fe8cb4c8e33e5427e268bb0fbef
Alex Garipian <agaripian@gmail.com>
2017-08-12 11:24:05 -0400

    Remove Bower and update dependencies

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,12 +1,12 @@
 require.config({
   baseUrl: '/',
   paths: {
-    base64: 'lib/base64/base64',
-    sinon: 'lib/sinonjs/sinon',
+    base64: 'node_modules/Base64/base64',
+    sinon: 'node_modules/sinon/lib/sinon',
     'sinon-chai': 'node_modules/sinon-chai/lib/sinon-chai',
     chai: 'node_modules/chai/chai',
-    inherits: 'lib/inherits/inherits',
-    'event-emitter': 'lib/event-emitter/src/event-emitter'
+    inherits: 'node_modules/inherits/inherits',
+    'event-emitter': 'node_modules/events-event-emitter/src/event-emitter'
   },
   packages: [{
     name: 'eref',
@@ -16,7 +16,7 @@ require.config({
     location: 'tests'
   },{
     name: "stream",
-    location: "lib/stream/src"
+    location: "node_modules/stream/src"
   }],
   shim: {
     'sinon': {
